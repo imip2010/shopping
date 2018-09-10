c1cb01817f418105edf1115d684a712dcd1ba70d
Galih Rahayu <galihrahayu@gmail.com>
2018-09-10 17:09:57 +0700

    10 september 2018 perbaiki front end lebel barang

--- application/views/labelbarang_v.php
+++ application/views/labelbarang_v.php
@@ -10,42 +10,27 @@
 <div class="col-lg-3 col-md-12">
     <div class="card">
         <div class="card-body">
-            <h4 class="card-title">Nestable1</h4>
             <div class="myadmin-dd dd" id="nestable">
                 <ol class="dd-list">
                     <li class="dd-item" data-id="1">
-                        <div class="dd-handle"> Item 1 </div>
+                        <div class="dd-handle"> Ringkasan Akun  </div>
                     </li>
                     <li class="dd-item" data-id="2">
-                        <div class="dd-handle"> Item 2 </div>
+                        <div class="dd-handle"> Daftar Barang </div>
                         <ol class="dd-list">
                             <li class="dd-item" data-id="3">
-                                <div class="dd-handle"> Item 3 </div>
+                                <div class="dd-handle"> Barang Dijual </div>
                             </li>
                             <li class="dd-item" data-id="4">
-                                <div class="dd-handle"> Item 4 </div>
+                                <div class="dd-handle"> Barang Tidak Dijual </div>
                             </li>
-                            <li class="dd-item" data-id="5">
-                                <div class="dd-handle"> Item 5 </div>
-                                <ol class="dd-list">
-                                    <li class="dd-item" data-id="6">
-                                        <div class="dd-handle"> Item 6 </div>
-                                    </li>
-                                    <li class="dd-item" data-id="7">
-                                        <div class="dd-handle"> Item 7 </div>
-                                    </li>
-                                    <li class="dd-item" data-id="8">
-                                        <div class="dd-handle"> Item 8 </div>
-                                    </li>
-                                </ol>
-                            </li>
-                            <li class="dd-item" data-id="9">
-                                <div class="dd-handle"> Item 9 </div>
-                            </li>
-                            <li class="dd-item" data-id="10">
-                                <div class="dd-handle"> Item 10 </div>
+                            <li class="dd-item" data-id="4">
+                                <div class="dd-handle"> Barang Draf </div>
                             </li>
                         </ol>
+                    </li>
+                    <li class="dd-item" data-id="1">
+                        <div class="dd-handle"> Label Barang  </div>
                     </li>
                 </ol>
             </div>
