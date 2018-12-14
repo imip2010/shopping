da992da7e13707a8c3a3db219f391304126c8bd1
Galih Rahayu <galihrahayu@gmail.com>
2018-12-14 16:39:08 +0700

    sds

--- application/views/labelbarang_v.php
+++ application/views/labelbarang_v.php
@@ -1,4 +1,7 @@
+
+
 <br>
+
 <!-- ============================================================== -->
 <!-- Start Page Content -->
 <!-- ============================================================== -->
@@ -13,126 +16,119 @@
                 <div class="text-center">
                     <button type="button" class="btn waves-effect waves-light btn-outline-info"> Jual Barang </button>
                 </div><br>
-                <ol class="dd-list">
-                    <li class="dd-item" >
+                <ul class="list-style-none el-info" >
+                    <li class="el-item"> 
                         <a href="#"> Ringkasan Akun </a>
-                    </li><br>
-                    <li class="dd-item">
-                        <a href="<?php echo base_url()?>labelbarang"  style="color: #4798e8;" > Label Barang </a>   
-                    </li><br>
-                    <li class="dd-item" >
-                        <a href="#"> Daftar Barang </a>
-                        <ol class="dd-list"><br>
-                            <li class="dd-item" >
-                                <a href="#"> Barang Dijual </a>
-                            </li><br>
-                            <li class="dd-item" >
-                                <a href="#"> Barang Tidak Dijual </a> 
-                            </li><br>
-                            <li class="dd-item" >
-                                <a href="#"> Barang Draf  </a>    
-                            </li><br>
-                        </ol>
                     </li>
-                    
+                    <li class="el-item">
+                        <a href="<?php echo base_url()?>labelbarang"  style="color: #4798e8;" > Label Barang </a>
+                    </li>
+                    <li class="el-item">
+                        <a class="card-link" data-toggle="collapse" href="#collapseOne"> Daftar Barang <i class="ti-angle-down" sty></i></a>
+                        <ul id="collapseOne" class="collapse show" data-parent="#accordion" style="margin-left: -20px;">
+                            <li>Barang Dijual</li>
+                            <li>Barang Tidak Dijual</li>
+                            <li>Barang Draf</li>
+                        </ul>
+                    </li>
                     <div class="dropdown-divider"></div>
 
-                    <li class="dd-item" data-id="1">
+                    <li  class="el-item">
                         <a href="<?php echo base_url()?>labelbarang"> Teman </a> 
-                    </li><br>
-                    <li class="dd-item" data-id="1">
-                        <a href="<?php echo base_url()?>labelbarang"> Barang Favorit </a> 
-                    </li><br>
-                    <li class="dd-item" data-id="1">
-                        <a href="<?php echo base_url()?>labelbarang"> Langganan </a>    
-                    </li><br>
-                    <li class="dd-item" data-id="1">
-                        <a href="<?php echo base_url()?>labelbarang"> Pembelian Rutin </a>   
                     </li>
-                    
-                    <div class="dropdown-divider"></div>
+                  <li  class="el-item">
+                      <a href="<?php echo base_url()?>labelbarang"> Barang Favorit </a> 
+                  </li>
+                  <li class="el-item">
+                      <a href="<?php echo base_url()?>labelbarang"> Langganan </a>    
+                  </li>
+                  <li class="el-item">
+                      <a href="<?php echo base_url()?>labelbarang"> Pembelian Rutin </a>   
+                  </li>
+                  
+                  <div class="dropdown-divider"></div>
 
-                    <li class="dd-item" data-id="1">
-                        <a href="<?php echo base_url()?>labelbarang"> Saldo </a>   
-                    </li><br>
-                    <li class="dd-item" data-id="1">
-                        <a href="<?php echo base_url()?>labelbarang"> Transaksi </a>   
-                    </li><br>
-                    <li class="dd-item" data-id="1">
-                        <a href="<?php echo base_url()?>labelbarang"> Feedback </a>   
-                    </li>
-                    
-                    <div class="dropdown-divider"></div>
-                    
-                    <li class="dd-item" data-id="1">
-                        <a href="<?php echo base_url()?>labelbarang"> Pesan </a>  
-                    </li><br>
-                    <li class="dd-item" data-id="1">
-                        <a href="<?php echo base_url()?>labelbarang"> Diskusi Barang </a>  
-                    </li>
-                    
-                    <div class="dropdown-divider"></div>
-                    
-                    <li class="dd-item" data-id="1">
-                        <a href="<?php echo base_url()?>labelbarang"> Kode Promo </a> 
-                    </li><br>
-                    <li class="dd-item" data-id="1">
-                        <a href="<?php echo base_url()?>labelbarang"> Koperasi Widget </a> 
-                    </li><br>
-                    <li class="dd-item" data-id="1">
-                        <a href="<?php echo base_url()?>pengaturan_profile"> Pengaturan </a>
-                    </li>
-                </ol><br>
-                <div class="text-center">
-                    <button type="button" class="btn waves-effect waves-light btn-outline-info"> Preview Lapak </button>
-                </div>
-            </div>
-        </div>
-    </div>
+                  <li class="el-item">
+                      <a href="<?php echo base_url()?>labelbarang"> Saldo </a>   
+                  </li>
+                  <li class="el-item">
+                      <a href="<?php echo base_url()?>labelbarang"> Transaksi </a>   
+                  </li>
+                  <li class="el-item">
+                      <a href="<?php echo base_url()?>labelbarang"> Feedback </a>   
+                  </li>
+                  
+                  <div class="dropdown-divider"></div>
+                  
+                  <li class="el-item">
+                      <a href="<?php echo base_url()?>labelbarang"> Pesan </a>  
+                  </li>
+                  <li class="el-item">
+                      <a href="<?php echo base_url()?>labelbarang"> Diskusi Barang </a>  
+                  </li>
+                  
+                  <div class="dropdown-divider"></div>
+                  
+                  <li  class="el-item">
+                      <a href="<?php echo base_url()?>labelbarang"> Kode Promo </a> 
+                  </li>
+                  <li  class="el-item"">
+                      <a href="<?php echo base_url()?>labelbarang"> Koperasi Widget </a> 
+                  </li>
+                  <li  class="el-item">
+                      <a href="<?php echo base_url()?>pengaturan_profile"> Pengaturan </a>
+                  </li>
+                </ul><br>
+              <div class="text-center">
+                  <button type="button" class="btn waves-effect waves-light btn-outline-info"> Preview Lapak </button>
+              </div>
+          </div>
+      </div>
+  </div>
 </div>
 
 <!-- Column bagian kiri : selasai -->
 
 <!-- Column bagian kanan : mulai -->
 <div class="col-lg-9 col-md-12">
-    <h3 class="card-title">Pengaturan Label Barang</h3><br>
-    <p >Label digunakan untuk mengelompokkan barang agar barang lebih mudah ditemukan oleh pembeli. Atur label di halaman ini dan <a href="" style="color: #4798e8;"> Pasang/Lepas label pada barang </a> di toko saya.</p>
-    <div class="row">
-        <div class="col-lg-11 col-md-12">
-            <div class="card border-info">
-                <div class="card-header bg-info">
-                    <h4 class="m-b-0 text-white">Buat Label Baru</h4>
-                </div>
-                <div class="card-body">
-                <form class="form">
-                    <div class="form-group row">
-                        <label for="example-text-input" class="col-2 col-form-label">Nama Label</label>
-                        <div class="col-10">
-                            <input class="form-control" type="text" maxlength="30">
-                            <p>Maksimum 30 karakter</p>
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label for="example-search-input" class="col-2 col-form-label">Deskripsi</label>
-                        <div class="col-10">
-                            <textarea class="form-control" maxlength="200"></textarea>
-                            <p class="text-right">200 karakter</p>
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label for="example-text-input" class="col-2 col-form-label"></label>
-                        <div class="col-10">
-                        <button type="submit" class="btn btn-lg btn-info ">Simpan</button>
-                        </div>
-                    </div>
-                    
-                </form>
-                </div>
-            </div>
-        </div>
-    </div>
-        </div>
-    </div>
+  <h3 class="card-title">Pengaturan Label Barang</h3><br>
+  <p >Label digunakan untuk mengelompokkan barang agar barang lebih mudah ditemukan oleh pembeli. Atur label di halaman ini dan <a href="" style="color: #4798e8;"> Pasang/Lepas label pada barang </a> di toko saya.</p>
+  <div class="row">
+      <div class="col-lg-11 col-md-12">
+          <div class="card border-info">
+              <div class="card-header bg-info">
+                  <h4 class="m-b-0 text-white">Buat Label Baru</h4>
+              </div>
+              <div class="card-body">
+              <form class="form">
+                  <div class="form-group row">
+                      <label for="example-text-input" class="col-2 col-form-label">Nama Label</label>
+                      <div class="col-10">
+                          <input class="form-control" type="text" maxlength="30">
+                          <p>Maksimum 30 karakter</p>
+                      </div>
+                  </div>
+                  <div class="form-group row">
+                      <label for="example-search-input" class="col-2 col-form-label">Deskripsi</label>
+                      <div class="col-10">
+                          <textarea class="form-control" maxlength="200"></textarea>
+                          <p class="text-right">200 karakter</p>
+                      </div>
+                  </div>
+                  <div class="form-group row">
+                      <label for="example-text-input" class="col-2 col-form-label"></label>
+                      <div class="col-10">
+                      <button type="submit" class="btn btn-lg btn-info ">Simpan</button>
+                      </div>
+                  </div>
+                  
+              </form>
+              </div>
+          </div>
+      </div>
+  </div>
+      </div>
+  </div>
 </div>
 <!-- Column bagian kanan : selesai -->
 
