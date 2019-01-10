cfdd94f85c350719daba92fabae9abdbb0b603cc
Galih Rahayu <galihrahayu@gmail.com>
2019-01-10 11:45:18 +0700

    perubahan riwayat belanja

--- application/views/member/Riwayatbelanja.php
+++ application/views/member/Riwayatbelanja.php
@@ -57,13 +57,13 @@
 
     <!-- Column bagian kanan : mulai -->
     <div class="col-lg-10 col-md-12">
-        <h3 class="card-title">Riwayat Belanjaan </h3><br>
+        <h3 class="card-title">Riwayat Belanjaan </h3>
         <div class="col-lg-12 col-md-12 col-sm-12">
             <ul class="nav nav-pills m-t-30 m-b-30">
                 <li class=" nav-item"> <a href="#navpills-1" class="nav-link active" data-toggle="tab" aria-expanded="false">Belum Bayar</a> </li>
-                <li class="nav-item"> <a href="#navpills-2" class="nav-link" data-toggle="tab" aria-expanded="false">Belum Dikirimkan</a> </li>
-                <li class="nav-item"> <a href="#navpills-3" class="nav-link" data-toggle="tab" aria-expanded="true">Belum Diterima</a> </li>
-                <li class=" nav-item"> <a href="#navpills-4" class="nav-link" data-toggle="tab" aria-expanded="false">Pembelian Selesai</a> </li>
+                <li class="nav-item"> <a href="#navpills-2" class="nav-link" data-toggle="tab" aria-expanded="false">Dikemas</a> </li>
+                <li class="nav-item"> <a href="#navpills-3" class="nav-link" data-toggle="tab" aria-expanded="true">Dikirim</a> </li>
+                <li class=" nav-item"> <a href="#navpills-4" class="nav-link" data-toggle="tab" aria-expanded="false">Selesai</a> </li>
                 <li class="nav-item"> <a href="#navpills-5" class="nav-link" data-toggle="tab" aria-expanded="false">Batal</a> </li>
             </ul>
 
@@ -74,20 +74,20 @@
                 <div id="navpills-1" class="tab-pane active">
                     <div class="row">
                         <div class="table-responsive">
-                            <h5 class="card-title">Profil Akun </h5>
-                            <p> Data Anda selalu rahasia dan tidak akan kami beritahukan kepada pihak ketiga.</p>
                             <form action=" " method="post">
                                 <table class="table">
                                     <tbody>
                                         <tr>
-                                            <td ><h4>Informasi Umum</h4></td>
-                                            <td class="text-right">
+                                            <td class="text-center"><h5>Nama Toko</h5></td>
+                                            <td class="text-left">
+                                                <button type="button" class="btn waves-effect waves-light btn-info"><span class="btn-label"><i class="mdi mdi-message"></i></span> Chat </button>
+                                                <button type="button" class="btn waves-effect waves-light btn-outline-info"> Kunjungi Toko </button>
                                             </td>
-                                            <td></td>
+                                            <td><h4 style="color: #4798e8;">Belum Bayar</h4></td>
                                         </tr>
                                         <tr>
-                                            <td style="width: 30%"> <img src="<?php echo base_url()?>assets/images/img3.jpg" style="width: 50%"/></td>
-                                            <td style="width: 50%">Nama Produk <p>Jumlah produk</p> </td>
+                                            <td style="width: 10%"> <img src="<?php echo base_url()?>assets/images/img3.jpg" style="width: 100%"/></td>
+                                            <td style="width: 75%">Nama Produk <p>Jumlah produk</p> </td>
                                             <td >Harga Produk</td>
                                         </tr>
                                         <tr>
@@ -96,7 +96,16 @@
                                             <td><h4>Rp 200.000.000 </h4></td>
                                         </tr>
                                     </tbody>
-                                </table>
+                                </table><br>
+                                <div class="text-right" style="margin-right: 20px;">
+                                    <button type="button" class="btn waves-effect waves-light btn-info"> Transfer Sekarang </button>
+                                    <button type="button" class="btn waves-effect waves-light btn-outline-info"> Tampilkan Rincian Barang </button>
+                                    <button class="btn waves-effect waves-light btn-outline-info dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Lainnya</button>
+                                    <div class="dropdown-menu">
+                                        <a class="dropdown-item" href="javascript:void(0)">Batalkan Pesanan</a>
+                                        <a class="dropdown-item" href="javascript:void(0)">Ubah Metode Pembayaran</a>
+                                    </div>
+                                </div>
                             </form><br><br>
                         </div>
                     </div>
