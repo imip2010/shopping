e2eab955860ca557e299e1d041589153ff1dc1ba
pepypeppep <pepy.peppep@gmail.com>
2019-01-15 13:03:14 +0700

    create transactions history function and page

--- application/views/member/pengaturan_profile_v.php
+++ application/views/member/pengaturan_profile_v.php
@@ -4,87 +4,9 @@
 <!-- ============================================================== -->
 <div class="row">
 
-    <!-- Column bagian kiri : mulai -->
-
-<div class="col-lg-2 col-md-12">
-    <div class="card">
-        <div class="card-body">
-            <div class="myadmin-dd dd" id="nestable">
-                <div class="text-center">
-                    <button type="button" class="btn waves-effect waves-light btn-outline-info"> Jual Barang </button>
-                </div><br>
-                <ul class="list-style-none el-info" >
-                    <li class="el-item"> 
-                        <a href="#"> Ringkasan Akun </a>
-                    </li>
-                    <li class="el-item">
-                        <a href="<?php echo base_url()?>labelbarang"  style="color: #4798e8;" > Label Barang </a>
-                    </li>
-                    <li class="el-item">
-                        <a class="card-link" data-toggle="collapse" href="#collapseOne"> Daftar Barang <i class="ti-angle-down" style="padding: 55px;"></i></a>
-                        <ul id="collapseOne" class="collapse show" data-parent="#accordion" style="margin-left: -20px;">
-                            <li>Barang Dijual</li>
-                            <li>Barang Tidak Dijual</li>
-                            <li>Barang Draf</li>
-                        </ul>
-                    </li>
-                    <div class="dropdown-divider"></div>
-
-                    <li  class="el-item">
-                        <a href="<?php echo base_url()?>labelbarang"> Teman </a> 
-                    </li>
-                  <li  class="el-item">
-                      <a href="<?php echo base_url()?>labelbarang"> Barang Favorit </a> 
-                  </li>
-                  <li class="el-item">
-                      <a href="<?php echo base_url()?>labelbarang"> Langganan </a>    
-                  </li>
-                  <li class="el-item">
-                      <a href="<?php echo base_url()?>labelbarang"> Pembelian Rutin </a>   
-                  </li>
-                  
-                  <div class="dropdown-divider"></div>
-
-                  <li class="el-item">
-                      <a href="<?php echo base_url()?>labelbarang"> Saldo </a>   
-                  </li>
-                  <li class="el-item">
-                      <a href="<?php echo base_url()?>labelbarang"> Transaksi </a>   
-                  </li>
-                  <li class="el-item">
-                      <a href="<?php echo base_url()?>labelbarang"> Feedback </a>   
-                  </li>
-                  
-                  <div class="dropdown-divider"></div>
-                  
-                  <li class="el-item">
-                      <a href="<?php echo base_url()?>labelbarang"> Pesan </a>  
-                  </li>
-                  <li class="el-item">
-                      <a href="<?php echo base_url()?>labelbarang"> Diskusi Barang </a>  
-                  </li>
-                  
-                  <div class="dropdown-divider"></div>
-                  
-                  <li  class="el-item">
-                      <a href="<?php echo base_url()?>labelbarang"> Kode Promo </a> 
-                  </li>
-                  <li  class="el-item"">
-                      <a href="<?php echo base_url()?>labelbarang"> Koperasi Widget </a> 
-                  </li>
-                  <li  class="el-item">
-                      <a href="<?php echo base_url()?>pengaturan_profile"> Pengaturan </a>
-                  </li>
-                </ul><br>
-              <div class="text-center">
-                  <button type="button" class="btn waves-effect waves-light btn-outline-info"> Preview Lapak </button>
-              </div>
-          </div>
-      </div>
-  </div>
-</div>
-
-    <!-- Column bagian kiri : selasai -->
+    <?php
+        echo $sidebar;
+    ?>
 
     <!-- Column bagian kanan : mulai -->
     <div class="col-lg-10 col-md-12">
